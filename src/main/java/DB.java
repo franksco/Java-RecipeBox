6ca9f286fbb58e4a4c5cd80672631a38ca69ec15
Cory Franks and Garrett Biernat <cory+garrett@>
2016-05-12 16:51:40 -0700

    revert back to local database

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-243-62-211.compute-1.amazonaws.com:5432/dc86cpp17re3td?sslmode=require&user=edxwytyvuxklsf&password=F5ntGke3HaVNvoWXsE2e9RcaLK", null, null);
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/recipe_box", null, null);
 }
